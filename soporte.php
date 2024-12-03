202f1f7a011ca4b524315f3d896f2bcb4c7a4d46
DpalaciosR20 <diegopalaciosrobles@hotmail.com>
2024-12-03 13:58:27 -0600

    act

--- soporte.php
+++ soporte.php
@@ -7,13 +7,13 @@ require 'includes/funciones.php';
 incluirTemplate('header');
 
 // Solo para las secciones que son exclusivas de un usuario loggeado
-// if (!$_SESSION['login']) {
-//     header('Location: /');
-// }
+if (!$_SESSION['login']) {
+    header('Location: /');
+}
 ?>
 
-<main class="contenedor seccion">
-    <h1 class="titulo"></h1>
+<main class="contenedor seccion contenedor-soporte">
+    <h1 class="titulo-soporte">¿En qué podemos ayudarte?</h1>
     <p class="descripcion">
         
     </p>
