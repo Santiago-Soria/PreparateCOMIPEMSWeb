6a3b9f5ce36a821c3421b86550d3c63e24ee3952
DpalaciosR20 <diegopalaciosrobles@hotmail.com>
2024-12-02 18:02:24 -0600

    act

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -1,3 +1,11 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+$auth = $_SESSION['login'] ?? false;
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -6,9 +14,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PrepárateCOMIPEMS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="build/css/app.css">
-    <link rel="shortcut icon" href="src/img/icono_pestana.svg">
+    <link rel="shortcut icon" href="build/img/icono_pestana.svg">
 </head>
 
 <body>
@@ -16,19 +23,28 @@
         <div class="contenedor contenido-header">
             <div class="barra">
                 <a href="/">
-                    <img src="src/img/logo.png" alt="Logotipo de PrepárateCOMIPEMS" class="logo">
+                    <img src="build/img/logo.png" alt="Logotipo de PrepárateCOMIPEMS" class="logo">
                 </a>
 
                 <nav class="navegacion">
-                    <a href="#">Inicio</a>
-                    <a href="#" class="active">Guía Dígital</a>
-                    <a href="#">Exámenes</a>
-                    <a href="#">Mi Progrso</a>
-                    <a href="#">Comunidad</a>
-                    <a href="#">Soporte</a>
-                    <a href="#">
-                        <img src="src/img/iniciar_sesion.png" alt="Ícono de Inicio de Sesión" width="32" height="32">
-                    </a>
+                    <?php if ($auth): ?>
+                        <a href="#">Inicio</a>
+                        <a href="#" class="active">Guía Dígital</a>
+                        <a href="#">Exámenes</a>
+                        <a href="#">Mi Progrso</a>
+                        <a href="#">Comunidad</a>
+                        <a href="#">Soporte</a>
+                        <a href="cerrar-sesion.php">
+                            <img src="/build/img/logout.svg" alt="Cerrar sesión" title="Cerrar sesión">
+                        </a>
+                    <?php else: ?>
+                        <a href="#">Guía digital</a>
+                        <a href="#">Examenes Simulacro</a>
+                        <a href="#">Información sobre COMIPEMS</a>
+                        <a href="iniciar-sesion.php">
+                            <img src="/build/img/login.svg" alt="Iniciar sesión" title="Iniciar sesión">
+                        </a>
+                    <?php endif; ?>
                 </nav>
             </div> <!-- .barra -->
         </div>
--- index.php
+++ index.php
@@ -1,29 +1,9 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PrepárateCOMIPEMS</title>
-   
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,700&display=swap" rel="stylesheet">
-   
-    <link rel="stylesheet" href="../Estilos/estilo_bienvenida.css">
-</head>
+<?php
+require 'includes/funciones.php';
+incluirTemplate('header');
+?>
 
 <body>
-   
-<header class="header">
-    <div class="logo">
-        <img src="../Recursos/logo.png" alt="Logo del Programa">
-    </div>
-    <div class="divider"></div>
-    <nav class="nav-buttons">
-        <a href="#" class="nav-button">Guía digital</a>
-        <a href="#" class="nav-button">Exámenes simulacro</a>
-        <a href="#" class="nav-button">Información sobre COMIPEMS</a>
-        <a href="/ProgramaFEPI/Pantallas/iniciar_sesion.php" class="nav-button">Iniciar sesión</a>
-    </nav>
-</header>
 
 <div class="container">
     <div class="Texto-Bienvenida">
@@ -32,7 +12,7 @@
 
     <div class="descripcion-con-imagen">
         <div class="imagen-plataforma">
-            <img src="../Recursos/escuelasycomipems.png" alt="Imagen descriptiva" />
+            <img src="build/img/escuelasycomipems.png" alt="Imagen descriptiva" />
         </div>
         <div class="descripcion-plataforma">
             En esta plataforma, tienes acceso al temario oficial del COMIPEMS y una serie de exámenes simulacro diseñados para ayudarte a identificar tus fortalezas y debilidades.
@@ -47,6 +27,6 @@
     </div>
 </div>
 
-<!-- Actualizate ya xd -->
-<footer class="footer">Derechos reservados a "PreparateCOMIPEMS"</footer>
-</body>
+<?php
+incluirTemplate('footer');
+?>
