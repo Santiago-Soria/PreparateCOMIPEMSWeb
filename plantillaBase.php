ac7859e4b681360ea11012ac03dc0910163e71a2
DpalaciosR20 <diegopalaciosrobles@hotmail.com>
2024-12-03 13:20:01 -0600

    act

--- plantillaBase.php
+++ plantillaBase.php
@@ -12,6 +12,16 @@ incluirTemplate('header');
 // }
 ?>
 
+<main class="contenedor seccion">
+    <h1 class="titulo">Progreso Asignarura</h1>
+    <p class="descripcion">
+        En esta sección podrás consultar tu progreso detallado en cada una de las asignaturas que forman parte del examen COMIPEMS. Se presenta un desglose por temas, permitiéndote identificar las áreas en las que has avanzado y aquellas que requieren mayor enfoque. Utiliza esta herramienta para gestionar tu estudio de manera eficiente y optimizar tu rendimiento académico.
+    </p>
+</main>
+
+<div class="contenedor seccion">
+</div>
+
 <?php
 incluirTemplate('footer');
 ?>
